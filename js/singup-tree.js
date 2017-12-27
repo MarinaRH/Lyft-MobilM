74e40a0ba553fc55c6ee942d9c00db0a4b4ecb29
MarinaRH <l_marina1717@hotmail.com>
2017-12-27 13:05:36 -0500

    Agregando funcion para validar formulario

--- js/singup-tree.js
+++ js/singup-tree.js
@@ -3,26 +3,55 @@ $(document).ready(function(){
   var $name = $('#name');
   var $last = $('#last');
   var $email = $('#email');
-  var $checkbox = $('input'[type="checkbox"]);
+  var $checked = $('input[type="checkbox"]');
   var $btn = $('#btn-next');
+  var validateName = false;
+  var validateLast = false; 
+  var validateEmail = false;
+  var validateChecked = false; 
+
 
   $name.on('input',function(){
     if($(this).val()){
-       activeButton();
+      validateName = true;
+      activeButton();
     }
   });
 
   $last.on('input',function(){
     if($(this).val()){
-       activeButton();
+      validateLast = true;
+      activeButton();
     }
   });
-   
 
-  function activeButton(){
-    if($name===true || $last===true ){
-        $btn.attr('disabled',false);
+  $email.on('input', function(event) {
+    var REGEXEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    if (REGEXEMAIL.test($(this).val())) {
+      validateEmail = true;
+      activeButton(); 
+    }
+  });
+
+  $checked.on('click', function(event) {
+    if(event.target.checked) {
+      validateChecked = true;
+      activeButton();
+    } 
+  });
+
+  function activeButton() {
+    if (validateName && validateLast && validateEmail && validateChecked) {
+      $btn.attr('disabled', false);
+      $btn.addClass('btn-grad');
+    }else {
+      $btn.attr('disabled', true);
     }
   }
+
+  $btn.on('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'check.html';
+  });
 
 });
